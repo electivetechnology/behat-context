6090de73e9ca5c6751ef04728e88fa69928bc1e6
krisrybak <kris.rybak@hotmail.co.uk>
2019-03-29 14:31:58 +0000

    Adds Request/Response

--- Context/RestContext.php
+++ Context/RestContext.php
@@ -4,6 +4,9 @@ namespace Elective\BehatContext\Context;
 
 use Behat\Behat\Context\Context;
 use Symfony\Component\HttpKernel\KernelInterface;
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
 
 /**
  * Elective\BehatContext\Context\RestContext
@@ -15,6 +18,16 @@ class RestContext implements Context
      */
     private $kernel;
 
+    /**
+     * @var Response
+     */
+    private $response;
+
+    /**
+     * @var Request
+     */
+    private $request;
+
     public function __construct(KernelInterface $kernel)
     {
         $this->kernel = $kernel;
@@ -23,6 +36,37 @@ class RestContext implements Context
     public function getKernel(): KernelInterface
     {
         return $this->kernel;
+    }
+
+    public function setKernel(KernelInterface $kernel): self
+    {
+        $this->kernel = $kernel;
+
+        return $this;
+    }
+
+    public function getResponse(): Response
+    {
+        return $this->response;
+    }
+
+    public function setResponse(Response $response): self
+    {
+        $this->response = $response;
+
+        return $this;
+    }
+
+    public function getRequest(): Request
+    {
+        return $this->request;
+    }
+
+    public function setRequest(Request $request): self
+    {
+        $this->request = $request;
+
+        return $this;
     }
 
     /**
